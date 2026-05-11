d5b5f0c80c55b4484f8b245e255f689118af6c05
Zsolt Bertalan <herrbert74@gmail.com>
2026-05-11 18:02:09 +0100

    Fix typesafe conventions plugin; remove some upgrades and dependencies

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -14,11 +14,6 @@ dependencyResolutionManagement {
 		google()
 		maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 	}
-	versionCatalogs {
-		create("libs") {
-			from(files("../gradle/libs.versions.toml"))
-		}
-	}
 }
 
 plugins {
