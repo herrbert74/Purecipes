595a81ebec6b71ec377c13995478df2642c34793
Zsolt Bertalan <herrbert74@gmail.com>
2026-05-18 15:57:37 +0100

    Fix missing import

--- feature/recipedetails/ui/src/commonMain/kotlin/app/purecipes/feature/recipedetails/ui/RecipeDetailsScreen.kt
+++ feature/recipedetails/ui/src/commonMain/kotlin/app/purecipes/feature/recipedetails/ui/RecipeDetailsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
