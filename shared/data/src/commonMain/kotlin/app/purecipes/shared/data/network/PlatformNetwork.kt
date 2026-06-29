9b33d1a6d063a861288a7cdc0be0272db74c4432
Zsolt Bertalan <herrbert74@gmail.com>
2026-06-29 18:58:13 +0100

    Switch to https in the app as well

--- shared/data/src/commonMain/kotlin/app/purecipes/shared/data/network/PlatformNetwork.kt
+++ shared/data/src/commonMain/kotlin/app/purecipes/shared/data/network/PlatformNetwork.kt
@@ -5,7 +5,7 @@ import app.purecipes.shared.data.config.PurecipesBuildType
 internal const val DEBUG_BACKEND_PORT = 9090
 
 private const val STAGING_BACKEND_BASE_URL = "http://staging.purecipes.com/"
-private const val RELEASE_BACKEND_BASE_URL = "http://185.132.41.132" // "http://www.purecipes.app/"
+private const val RELEASE_BACKEND_BASE_URL = "https://purecipes.app/"
 
 fun backendBaseUrl(
 	buildType: PurecipesBuildType,
