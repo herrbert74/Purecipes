0eafc096c9b8c5e68dd49053ff160f4fa6f4808a
Zsolt Bertalan <herrbert74@gmail.com>
2026-07-24 17:54:37 +0100

    Fix major security issues

--- shared/data/src/commonMain/kotlin/app/purecipes/shared/data/network/PlatformNetwork.kt
+++ shared/data/src/commonMain/kotlin/app/purecipes/shared/data/network/PlatformNetwork.kt
@@ -4,7 +4,7 @@ import app.purecipes.shared.data.config.PurecipesBuildType
 
 internal const val DEBUG_BACKEND_PORT = 9090
 
-private const val STAGING_BACKEND_BASE_URL = "http://staging.purecipes.com/"
+private const val STAGING_BACKEND_BASE_URL = "https://staging.purecipes.app/"
 private const val RELEASE_BACKEND_BASE_URL = "https://purecipes.app/"
 
 fun backendBaseUrl(
